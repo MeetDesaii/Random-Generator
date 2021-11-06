0f1a7a26ac7a12191fe9f61036abc4287ed61818
Meet Desai <meetdesai3311@gmail.com>
2021-11-06 13:19:11 +0530

    Logo added

---
+++ app/src/main/java/com/codexblackpearl/practicemakesperfect/SplashDisplay.java
@@ -0,0 +1,24 @@
+package com.codexblackpearl.practicemakesperfect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_display);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashDisplay.this,MainActivity.class);
+            startActivity(intent);
+
+            finish();
+
+        },2000);
+    }
+}
